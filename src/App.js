10877dc85c1fea417450ce25211d3be008a86e70
Ata Ur Rehman Malik <119836227+arkhan998@users.noreply.github.com>
2024-10-28 18:15:49 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import AdminPanel from './AdminPanel';
 
 function App() {
-    return (
-        <div>
-            <AdminPanel />
-        </div>
-    );
+  return (
+    <div>
+      <h1>Admin Panel</h1>
+      <ul>
+        <li><a href="#add-product">Add Product</a></li>
+        <li><a href="#manage-themes">Manage Themes</a></li>
+        <li><a href="#manage-plugins">Manage Plugins</a></li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
